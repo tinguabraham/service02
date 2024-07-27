e1204c45ed4c3aebf5750cc0edc7e4fb635c8492
Amin Najji <amin.najji@gmail.com>
2024-07-27 11:03:36 +0200

    Modified train.py

--- src/model/train.py
+++ src/model/train.py
@@ -4,15 +4,16 @@ import argparse
 import glob
 import os
 
+import mlflow
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,11 +35,33 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X, y = (
+        df[
+            [
+                "Pregnancies",
+                "PlasmaGlucose",
+                "DiastolicBloodPressure",
+                "TricepsThickness",
+                "SerumInsulin",
+                "BMI",
+                "DiabetesPedigree",
+                "Age",
+            ]
+        ].values,
+        df["Diabetic"].values,
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0
+    )
+
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
@@ -46,16 +69,15 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument("--training_data", dest="training_data", type=str)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
