9c112fe03602df96786e6b8bc0d480d2663da0df
tinguabraham <tinguabraham@gmail.com>
2025-02-09 15:11:26 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -5,6 +5,7 @@ import glob
 import os
 
 import pandas as pd
+import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 
@@ -23,6 +24,12 @@ def main(args):
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
+def split_data:
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    len(X)
+    print(np.unique(y, return_counts=True))
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
 def get_csvs_df(path):
     if not os.path.exists(path):
